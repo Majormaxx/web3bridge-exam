4a3fa7329d013c9424ca1abec2789f75d8bdea63
Majormaxx <emersondaniel300@gmail.com>
2025-05-23 16:40:19 +0100

    Worked on my Readme

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <SavingContext>
-      <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-screen bg-gray-400 py-10 px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-3xl font-bold text-center mb-8">Savings Group App</h1>
           <Dashboard />
